f675db060b4bc5c765767bf1305429d7695bde0a
Wu_Being <1040003585@qq.com>
2018-01-12 13:18:38 +0800

    Ox at 12306 ^_^

--- MyExtensions/12306Ox/myOx.js
+++ MyExtensions/12306Ox/myOx.js
@@ -1,21 +1,22 @@
 /*
-    Ox at 12306 ... 
+    Ox at 12306 ... ^_^
 */
 
-var t_search = setInterval(search_ticket, 1000);
-var t_book   = setInterval(  book_ticket, 100);
+var t_search = setInterval(search_ticket, 2000);
+var t_book   = setInterval(  book_ticket, 200);
 var t_border = setInterval(change_color , 500);
 
 function search_ticket(){
-	var from = document.getElementById("fromStationText");
-	var to   = document.getElementById("toStationText");
+	//var from = document.getElementById("fromStationText");
+	//var to   = document.getElementById("toStationText");
 	var date = document.getElementById("train_date");
-	var student = document.getElementById("sf2");
+	//var student = document.getElementById("sf2");
 	var find1 = document.getElementById("query_ticket");
 	var find2 = document.getElementById("a_search_ticket");
+
 	//if(from) from.value = "上海";	//Todo: .click()
 	//if(to) to.value = "汕尾";	//Todo: .click()
-	if(date) date.value = "2018-02-01";
+	if(date) date.value = "2018-02-10";
 	//if(student) student.checked = true;
 	if(find1) find1.click();
 	if(find2) find2.click();
